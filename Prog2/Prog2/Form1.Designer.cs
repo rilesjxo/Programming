f9cc70815721ecd7dd8c95d1a36fbf3909d00e97
Riley Jordan <rilesjxo@gmail.com>
2024-11-17 22:19:38 -0500

    Add files via upload

---
+++ Prog2/Prog2/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Prog2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guestsLabel = new System.Windows.Forms.Label();
+            this.nightsLabel = new System.Windows.Forms.Label();
+            this.starsLabel = new System.Windows.Forms.Label();
+            this.costLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.guestsTextBox = new System.Windows.Forms.TextBox();
+            this.nightsTextBox = new System.Windows.Forms.TextBox();
+            this.starsTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // guestsLabel
+            // 
+            this.guestsLabel.AutoSize = true;
+            this.guestsLabel.Location = new System.Drawing.Point(48, 32);
+            this.guestsLabel.Name = "guestsLabel";
+            this.guestsLabel.Size = new System.Drawing.Size(95, 13);
+            this.guestsLabel.TabIndex = 0;
+            this.guestsLabel.Text = "Number of Guests:";
+            // 
+            // nightsLabel
+            // 
+            this.nightsLabel.AutoSize = true;
+            this.nightsLabel.Location = new System.Drawing.Point(48, 67);
+            this.nightsLabel.Name = "nightsLabel";
+            this.nightsLabel.Size = new System.Drawing.Size(92, 13);
+            this.nightsLabel.TabIndex = 1;
+            this.nightsLabel.Text = "Number of Nights:";
+            // 
+            // starsLabel
+            // 
+            this.starsLabel.AutoSize = true;
+            this.starsLabel.Location = new System.Drawing.Point(48, 104);
+            this.starsLabel.Name = "starsLabel";
+            this.starsLabel.Size = new System.Drawing.Size(62, 13);
+            this.starsLabel.TabIndex = 2;
+            this.starsLabel.Text = "Hotel Stars:";
+            // 
+            // costLabel
+            // 
+            this.costLabel.AutoSize = true;
+            this.costLabel.Location = new System.Drawing.Point(48, 228);
+            this.costLabel.Name = "costLabel";
+            this.costLabel.Size = new System.Drawing.Size(59, 13);
+            this.costLabel.TabIndex = 3;
+            this.costLabel.Text = "Hotel Cost:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.outputLabel.Location = new System.Drawing.Point(149, 224);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(100, 23);
+            this.outputLabel.TabIndex = 5;
+            // 
+            // guestsTextBox
+            // 
+            this.guestsTextBox.Location = new System.Drawing.Point(149, 30);
+            this.guestsTextBox.Name = "guestsTextBox";
+            this.guestsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.guestsTextBox.TabIndex = 6;
+            // 
+            // nightsTextBox
+            // 
+            this.nightsTextBox.Location = new System.Drawing.Point(149, 64);
+            this.nightsTextBox.Name = "nightsTextBox";
+            this.nightsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nightsTextBox.TabIndex = 7;
+            // 
+            // starsTextBox
+            // 
+            this.starsTextBox.Location = new System.Drawing.Point(149, 101);
+            this.starsTextBox.Name = "starsTextBox";
+            this.starsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.starsTextBox.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 283);
+            this.Controls.Add(this.starsTextBox);
+            this.Controls.Add(this.nightsTextBox);
+            this.Controls.Add(this.guestsTextBox);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.costLabel);
+            this.Controls.Add(this.starsLabel);
+            this.Controls.Add(this.nightsLabel);
+            this.Controls.Add(this.guestsLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Program 2 - Hotel Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label guestsLabel;
+        private System.Windows.Forms.Label nightsLabel;
+        private System.Windows.Forms.Label starsLabel;
+        private System.Windows.Forms.Label costLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.TextBox guestsTextBox;
+        private System.Windows.Forms.TextBox nightsTextBox;
+        private System.Windows.Forms.TextBox starsTextBox;
+    }
+}
+
---
+++ Prog2/Prog2/Form1.cs
@@ -0,0 +1,87 @@
+﻿///////////////////////////////////////////////////////////
+/// Lab #:       Program 2                              ///
+/// Section:     CIS 199-01                             ///
+/// Due Date:    3/10/2022                              ///
+/// Grading ID:  K3222                                  ///
+/// Description: A program that determines the cost of  ///
+///              a single hotel room based on number    ///
+///              of guests, duration of the stay, and   ///
+///              stars of the hotel.                    ///
+///////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int guests; // Variable for number of guests
+            int nights; // Variable for number of nights
+            int stars; // Variable for stars of the hotel
+            double guestflatrate = 0; // Variable for flat rate based on number of guests
+            double costpernight = 0; // Variable for nightly cost based on number of nights
+            double starfactor = 0; // Variable for star gactor based on stars of the hotel
+            double totalcost; // Variable for total cost of the stay
+
+            {
+                int.TryParse(guestsTextBox.Text, out guests); // Conversion for user input in guestsTextBox from string to integer
+
+                if (guests == 1) // If statement for 1 guest
+                    guestflatrate = 100; // Flat rate declaration for 1 guest
+                else if (guests == 2) // Else If statement for 2 guests
+                    guestflatrate = 150; // Flat rate declaration for 2 guests
+                else if (guests == 3) // Else If statement for 3 guests
+                    guestflatrate = 250; // Flat rate declaration for 3 guests
+                else if (guests >= 4 && guests <= 7) // Else If statement for 4- 7 guests
+                    guestflatrate = 400; // Flat rate declaration for 4 - 7 guests
+                else // Else statement for above if and if else statements for number of guests
+                    MessageBox.Show("Number of guests must be an integer between 1 and 7."); // Shows message box if user input is not an integer between 1 and 7
+
+                int.TryParse(nightsTextBox.Text, out nights); // Conversion for user input in nightsTextBox from string to integer
+
+                if (nights >= 1 && nights <= 6) // If statement for 1 - 6 nights
+                    costpernight = 100; // Cost per night declaration for 1 - 6 nights
+                else if (nights >= 7 && nights <= 30) // Else If statement for 7 - 30 nights
+                    costpernight = 75; // Cost per night declaration for 7 - 30 nights
+                else if (nights >= 31) // Else If statement for 31+ nights
+                    costpernight = 25; // Cost per night declaration for 31+ nights
+                else // Else statement for above if and if else statements for number of nights
+                    MessageBox.Show("Number of nights must be an integer greater than 0."); // Shows message box if user input is not an integer greater than 0
+
+                int.TryParse(starsTextBox.Text, out stars); // Conversion for user input in starsTextBox from string to integer
+
+                if (stars == 1) // If statement for 1 star
+                    starfactor = 1; // Star factor declarion for 1 star
+                else if (stars == 2) // Else If statement for 2 stars
+                    starfactor = 1.5; // Star factor declarion for 2 stars
+                else if (stars == 3) // Else If statement for 3 stars
+                    starfactor = 2.5; // Star factor declarion for 3 stars
+                else if (stars == 4) // Else If statement for 4 stars
+                    starfactor = 3; // Star factor declarion for 4 stars
+                else if (stars == 5) // Else If statement for 5 stars
+                    starfactor = 4; // Star factor declarion for 5 stars
+                else // Else statement for above if and if else statements for hotel stars
+                    MessageBox.Show("Hotel stars must be an integer between 1 and 5."); // Shows message box if user input is not an integer between 1 and 5
+            }
+
+            totalcost = (guestflatrate + nights * costpernight) * starfactor; // Calculates total cost of stay based on the formula (Flat Guest Fee + Number of Nights * Cost Per Night) * Star Factor)
+            outputLabel.Text = totalcost.ToString("C"); // Displays result of totalcost calculation in the outputLabel at the bottom of Form1
+
+        }
+    }
+}
